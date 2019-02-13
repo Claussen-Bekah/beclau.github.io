cbbee62bda0fc6a4f9d4894028fff02cf7b176ca
beclau <beclau@byui.edu>
2019-02-12 21:25:39 -0700

    new page

---
+++ assignments/lesson-6/js/currentdate.js
@@ -0,0 +1,12 @@
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+n = new Date();
+d = n.getDate();
+y = n.getFullYear();
+m = n.getMonth();
+w = n.getDay();
+
+document.getElementById("currentDate").innerHTML = weekdays[w] + ", " + d + " " + months[m] + " " + y;
+  

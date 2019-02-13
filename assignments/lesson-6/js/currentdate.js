b1f18a1d8d68f9d52db2449e5feca39307458a51
beclau <beclau@byui.edu>
2019-02-12 21:43:38 -0700

    syntax

--- assignments/lesson-6/js/currentdate.js
+++ assignments/lesson-6/js/currentdate.js
@@ -1,6 +1,6 @@
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 n = new Date();
 d = n.getDate();

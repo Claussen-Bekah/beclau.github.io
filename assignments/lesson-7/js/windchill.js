5d1d001009c6e73c87106495b8f22d3f57332054
beclau <beclau@byui.edu>
2019-02-22 22:24:48 -0700

    fixed reponsiveness

--- assignments/lesson-7/js/windchill.js
+++ assignments/lesson-7/js/windchill.js
@@ -9,7 +9,7 @@ function calcChill() {
 
  function windChill(tempF, speed) {
  	var temp = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * tempF) * (Math.pow(speed, 0.16));
- 	return (Math.round(temp * 10) / 10) + "&deg; F" ;
+ 	return (Math.round(temp * 10) / 10) + "&deg;" ;
  }
 
 

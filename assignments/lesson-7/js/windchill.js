30b015fb9770c85a5c6c281fa612a24da51a6ade
beclau <beclau@byui.edu>
2019-03-01 20:41:43 -0700

    fixed footer and some bugs

--- assignments/lesson-7/js/windchill.js
+++ assignments/lesson-7/js/windchill.js
@@ -1,15 +1,8 @@
 
-function calcChill() {
-	var a = parseFloat(document.getElementById('temperature').value);
-	var b = parseFloat(document.getElementById('speed').value);
-	var c = windChill(a, b);
-	
- 	document.getElementById('windOutput').innerHTML = c;
-}
+var temp = 35.74 + (0.6215 * 30) - (35.75 * Math.pow(10, 0.16)) + (0.4275 * 30) * (Math.pow(10, 0.16));
 
- function windChill(tempF, speed) {
- 	var temp = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * tempF) * (Math.pow(speed, 0.16));
- 	return (Math.round(temp * 10) / 10) + "&deg;" ;
- }
+document.getElementById("windOutput").innerHTML = (Math.round(temp * 10) / 10) + "&deg;" + "F";
+
+ 
 
 

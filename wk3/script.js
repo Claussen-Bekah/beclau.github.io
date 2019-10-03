b82f3ee3904d89e0d53c38942939486644835f9b
Bekah <beclau@byui.edu>
2019-10-03 12:39:37 -0600

    new dino page

--- wk3/script.js
+++ wk3/script.js
@@ -1,32 +1,41 @@
 var dinoArray = [{
         'name': 'Allosaurus',
-        'diet': 'carnivore'
+        'diet': 'carnivore',
+        'time': 'Jurassic'
     }, {
         'name': "T.Rex",
-        'diet': 'carnivore'
+        'diet': 'carnivore',
+        'time': 'Cretaceous' 
     }, {
         'name': "Ankylosaurus",
-        'diet': 'herbivore'
+        'diet': 'herbivore',
+        'time': 'Cretaceous' 
     }, {
         'name': "Ichthyosaurus",
-        'diet': 'carnivore'
+        'diet': 'carnivore',
+        'time': 'Jurassic'
     }, {
         'name': "Styracosaurus",
-        'diet': 'herbivore'
+        'diet': 'herbivore',
+        'time': 'Cretaceous'
     },
     {
         'name': "Brachiosaurus",
-        'diet': 'herbivore'
+        'diet': 'herbivore',
+        'time': 'Cretaceous' 
     },
     {
         'name': 'Velociraptor',
-        'diet': 'carnivore'
+        'diet': 'carnivore',
+        'time': 'Cretaceous' 
     }, {
         'name': "Ceratosaurus",
-        'diet': 'carnivore'
+        'diet': 'carnivore',
+        'time': 'Jurassic'
     }, {
         'name': "Lambeosaurus",
-        'diet': 'herbivore'
+        'diet': 'herbivore',
+        'time': 'Cretaceous' 
     }
 ];
 
@@ -41,18 +50,23 @@ function buildDino(dino) {
     div.setAttribute('class', 'cell');
     output.appendChild(div);
 
-    var h2 = document.createElement('h2');
-    h2.textContent = dino.name;
-    div.appendChild(h2);
 
     var img = document.createElement('img');
     img.setAttribute('src', 'img/' + dino.name.toLowerCase() + '.png');
     img.setAttribute('alt', 'picture of the mighty' + dino.name.toLowerCase());
     div.appendChild(img);
 
+    var h2 = document.createElement('h2');
+    h2.textContent = dino.name;
+    div.appendChild(h2);
+
     var h3 = document.createElement('h3');
     h3.textContent = dino.diet;
     div.appendChild(h3);
+
+    var h3Time = document.createElement('h3');
+    h3Time.textContent = dino.time;
+    div.appendChild(h3Time);
 
     if (dino.diet == 'herbivore') {
         var p = document.createElement('p');

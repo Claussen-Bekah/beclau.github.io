6636bd182155681619c8e1eb1dea3c05657e66f8
Bekah <beclau@byui.edu>
2019-11-14 20:21:48 -0700

    ajax call

---
+++ wk9/script.js
@@ -0,0 +1,31 @@
+function getQuote() {
+
+document.getElementById('button').classList.toggle('hidden');
+
+var request = new XMLHttpRequest();
+
+request.open("POST", "https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en&jsonp=?", true);
+
+request.onload = function() {
+    let quoteData = JSON.parse(request.responseText);
+
+    let quote = quoteData.quoteText;
+    let author = quoteData.quoteAuthor;
+
+    let quoteDiv = document.getElementById('quote');
+
+    let quoteBody = document.createElement('h1');
+    quoteBody.innerHTML = quote;
+    quoteDiv.appendChild(quoteBody);
+
+    let quoteAuthorHeader = document.createElement('h2');
+    quoteAuthorHeader.innerHTML = author;
+    quoteDiv.appendChild(quoteAuthorHeader);
+}
+
+request.send();
+}
+
+
+
+
